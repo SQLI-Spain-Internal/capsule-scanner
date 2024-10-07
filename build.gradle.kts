5a057e6b6c7532877c238fb834e5f7276219164a
Rodrigo Carreira <rodrigo.carreira@nestle.com>
2024-10-07 17:04:52 +0200

    Adding firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id ("io.realm.kotlin") version "1.11.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 val releaseDebuggable by extra(true)
